ce311de84f0bece9b4c490a358481f79b3191c32
Roman Truschev <truschev@gmail.com>
2012-01-30 10:52:30 +0400

    some changes

--- lib/config/sq_stream.rb
+++ lib/config/sq_stream.rb
@@ -11,3 +11,7 @@ require 'em-synchrony/em-redis'
 config['redis'] = EventMachine::Synchrony::ConnectionPool.new(:size => 20) do
   conn = EM::Protocols::Redis.connect('127.0.0.1', 6379) 
 end
+
+environment(:development) do
+  config['redis'].flushdb
+end
